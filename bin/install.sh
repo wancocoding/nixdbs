53abb05c8f0cd8a207d75fac639e2350cde032a7
coco <ergal@163.com>
2022-04-02 10:30:37 +0800

    add check bash on install script

--- bin/install.sh
+++ bin/install.sh
@@ -5,10 +5,22 @@ set -Eeu
 NIXDBS_REPO=git@192.168.0.118:dotfiles.git
 NIXDBS_HOME=$HOME/.nixdbs
 
+# check git
 if ! command -v git >/dev/null 2>&1; then
 	echo "you must install git first"
 fi
+# check bash
+if [ -z "${BASH_VERSION:-}" ]
+then
+  echo "Bash is required to interpret this script."
+  exit 1
+fi
 
+if [ -d $NIXDBS_HOME ];then
+	echo "nixdbs already exist!"
+	echo "if you want to reinstall it, you can remove $NIXDBS_HOME manually fist"
+	exit 1
+fi
 git clone $NIXDBS_REPO $NIXDBS_HOME
 
 /bin/bash $NIXDBS_HOME/bin/boot.sh
